0f84abcf5b4cf2d1a18de47442eb313d3a683ae8
Atheeque <atheequemf2003@gmail.com>
2025-02-22 00:25:20 +0530

    Deleted Radme

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,22 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
-
+ 
+export default clerkMiddleware({
+  // Array of routes to be ignored by the authentication middleware
+  publicRoutes: ["/", "/sign-in", "/sign-up"],
+  
+  // Optional: Prevent middleware running on static files & images
+  ignoredRoutes: [
+    "/((?!api|trpc))(_next|.+..+)",
+    "/api/webhooks(.*)",
+    "/favicon.ico",
+    "/images(.*)",
+  ],
+});
+ 
 export const config = {
-    matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        '/(api|trpc)(.*)',
-    ],
+  matcher: [
+    // Exclude files with . extension (e.g. images, CSS, JS)
+    "/((?!.*\\..*|_next).*)",
+    "/(api|trpc)(.*)"
+  ],
 };
