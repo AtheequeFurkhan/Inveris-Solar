064511c302713b70fefec2a9cc3f08f0dba0532d
Atheeque <atheequemf2003@gmail.com>
2025-02-22 17:04:37 +0530

    fixed middle ware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,10 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
- 
-export default clerkMiddleware({
-  // Array of routes to be ignored by the authentication middleware
-  publicRoutes: ["/", "/sign-in", "/sign-up"],
-  
-  // Optional: Prevent middleware running on static files & images
-  ignoredRoutes: [
-    "/((?!api|trpc))(_next|.+..+)",
-    "/api/webhooks(.*)",
-    "/favicon.ico",
-    "/images(.*)",
-  ],
-});
- 
+
+export default clerkMiddleware();
+
 export const config = {
-  matcher: [
-    // Exclude files with . extension (e.g. images, CSS, JS)
-    "/((?!.*\\..*|_next).*)",
-    "/(api|trpc)(.*)"
-  ],
+    matcher: [
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/(api|trpc)(.*)',
+    ],
 };
