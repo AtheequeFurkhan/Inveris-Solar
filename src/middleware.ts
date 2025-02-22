35413ec273a6c738c010bb0346ffa235b25c403d
Atheeque <atheequemf2003@gmail.com>
2025-02-23 00:41:48 +0530

    refactor middleware configuration for improved route matching and cleaner code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,25 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/sign-in",
+    "/sign-up",
+    "/api/webhooks(.*)",
+    "/docs(.*)"
+  ],
+  ignoredRoutes: [
+    "/((?!api|trpc))(_next|.+..+)",
+    "/api/webhooks(.*)",
+    "/favicon.ico",
+    "/images(.*)"
+  ],
+});
 
 export const config = {
-    matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        '/(api|trpc)(.*)',
-    ],
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/",
+    "/(api|trpc)(.*)"
+  ],
 };
